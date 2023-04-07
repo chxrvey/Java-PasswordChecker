725ccd520010a2107a07c61b22b3e6e929dc46d5
chxrvey <109527161+chxrvey@users.noreply.github.com>
2023-04-07 12:16:50 +0100

    save-3
    Created my Criteria 1 page and added comments to the page to highlight the rules needed to be followed.

---
+++ Criteria1.java
@@ -0,0 +1,18 @@
+/*  Criteria 1
+    GOOD PASSWORD
+    
+    - This is the Criteria for the "Good" Password, for this criteria to be met the following must be:
+
+    1. 8 to 10 Characters in length
+    2. Include 2 symbols, numbers or captials
+    3. None work related words such as: "Inconnection", "Events", "Hospitality", "name" or "DOB".
+
+    
+ */
+
+
+public class Criteria1 {
+
+    
+    
+}
